2a23715afd72f2e8f6dcd9b7be9f52f4b74ae666
umair055 <umairtariq403@gmail.com>
2023-12-03 10:20:31 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(express.static(path.resolve("")));
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
